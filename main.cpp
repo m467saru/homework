7dd95810731639025e245437abeb35bed928f3f7
osaru <yuuhisabu1192@gmail.com>
2015-04-13 20:36:39 +0900

    change debug mode
    不明なエラーが発生したのでdebugに関する文言を追加した

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 int main(){
 	
 	//int cnt = 0; //カウンタ変数
-	int box = 0; //入力用変数
-	int sum = 0; //入力された数を入れていく変数
-	int ans = 0; //あまりを入れる変数
+	float box = 0; //入力用変数
+	float sum = 0; //入力された数を入れていく変数
+	float ans = 0; //あまりを入れる変数
 	
 	printf("10以下の正の整数を入力してくださいね\n");
 	scanf("%d",&box);
-	
-	ans = box % 2;
-	if (ans = 0) print("偶数が入力されました。"); //偶数の場合：何もしない
+	printf("この時点でのbox...box=%d\n",box);
+	ans = box / ( box / 2 );
+	printf("この時点でのans...ans=%d\n",ans);
+	if (ans == 2) printf("偶数が入力されました。偶数の場合は演算を行いません。ans=%d",ans); //偶数の場合：何もしない
 		else { 
 			printf("奇数が入力されました%d\n",box);
 			sum = sum + box;
@@ -21,10 +22,10 @@ int main(){
 	
 		printf("10以下の正の整数を入力してください\n");
 		scanf("%d",&box);
-			ans = box % 2;
-		if (ans = 0) ; //偶数の場合：何もしない
+			ans = box / (box/2);
+		if (ans == 2) printf("forループ内で偶数が検出されました。"); //偶数の場合：何もしない
 			else { 
-				printf("奇数が入力されました%d\n",box);
+				printf("奇数が入力されましたbox=%d\n",box);
 				sum = sum + box;
 	
 			}
