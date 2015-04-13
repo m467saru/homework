049a6dc361f213131b9823d56c7a4dcaff771c0f
osaru <yuuhisabu1192@gmail.com>
2015-04-13 21:29:42 +0900

    bug fix
    バグフィックス。5で成功したが、4で成功しなかった。

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 int main(){
 	
 	//int cnt = 0; //カウンタ変数
-	float box = 0; //入力用変数
-	float sum = 0; //入力された数を入れていく変数
-	float ans = 0; //あまりを入れる変数
+	int box = 0; //入力用変数
+	int boxp = 0; //入力用変数（ポインタ）
+	int sum = 0; //入力された数を入れていく変数
+	int ans = 0; //あまりを入れる変数
 	
 	printf("10以下の正の整数を入力してくださいね\n");
 	scanf("%d",&box);
 	printf("この時点でのbox...box=%d\n",box);
-	ans = box / ( box / 2 );
+	ans = box % ( box / 2 );
 	printf("この時点でのans...ans=%d\n",ans);
 	if (ans == 2) printf("偶数が入力されました。偶数の場合は演算を行いません。ans=%d",ans); //偶数の場合：何もしない
 		else { 
@@ -21,8 +22,8 @@ int main(){
 	for (box=0;sum <= 20;box=0){
 	
 		printf("10以下の正の整数を入力してください\n");
-		scanf("%d",&box);
-			ans = box / (box/2);
+		box = scanf("%d",&boxp);
+			ans % box / (box/2);
 		if (ans == 2) printf("forループ内で偶数が検出されました。"); //偶数の場合：何もしない
 			else { 
 				printf("奇数が入力されましたbox=%d\n",box);
